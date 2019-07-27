f588f83195edb1f5b65742765df3bebdd5cb33e5
Jaehun Kim <crisplaver@gmail.com>
2019-07-27 14:36:53 +0900

    Add Color Classfication

--- sketch.js
+++ sketch.js
@@ -1,83 +1,153 @@
-const resolution = 40;
-let cols;
-let rows;
+let records = [
+    [255, 0, 0, 'red'],
+    [0, 255, 0, 'green'],
+    [0, 0, 255, 'blue'],
+    [255, 255, 0, 'yellow'],
+    [128, 0, 128, 'purple'],
+    [165, 42, 42, 'brown'],
+    [255, 192, 203, 'pink'],
+    [255, 165, 0, 'orange'],
+    [0, 0, 0, 'black'],
+    [255, 255, 255, 'white'],
+];
 
-const train_xs = tf.tensor2d([[0, 0], [0, 1], [1, 0], [1, 1]]);
-const train_ys = tf.tensor1d([0, 1, 1, 0]);
 
+let labelList = [
+    'red',
+    'green',
+    'blue',
+    'yellow',
+    'purple',
+    'brown',
+    'pink',
+    'orange',
+    'black',
+    'white'
+]
+
+let testColors = [];
+let xs, ys, xp;
 let model;
 
-const hidden = tf.layers.dense({
-    units: 4,
-    inputShape: [2],
-    activation: 'sigmoid'
-})
-
-const output = tf.layers.dense({
-    units: 1,
-    activation: 'sigmoid'
-})
-
-const optimizer = tf.train.adam(0.2);
-
-
-
-function train() {
-    const config = {
-        suffle: true,
-        epochs: 20
-    }
-    model.fit(train_xs, train_ys, config).then((res) => {
-        console.log(res.history.loss[0])
-        train();
-    });
-}
+let resolution = 100
 
 function setup() {
     createCanvas(400, 400);
+    
+
     cols = width / resolution;
     rows = height / resolution;
 
-    let inputs = [];
     for (let i = 0; i < rows; i++) {
         for (let j = 0; j < cols; j++) {
-            let x1 = i / rows;
-            let x2 = j / cols;
-            inputs.push([x1, x2])
+            let color = [random(255) / 255, random(255) / 255, random(255) / 255];
+            testColors.push(color)
         }
     }
-    xs = tf.tensor2d(inputs)
+
+    lossP = createP('loss');
+    epochP = createP('epochP');
+    for(let v of records){
+        createP(v)
+    }
+
+    let colors = [];
+    let labels = [];
+    for (let record of records) {
+        let color = [record[0] / 255, record[1] / 255, record[2] / 255];
+        colors.push(color);
+        let label = labelList.indexOf(record[3]);
+        labels.push(label)
+    }
+
+    xs = tf.tensor2d(colors);
+    let labelt = tf.tensor1d(labels, 'int32');
+    ys = tf.oneHot(labelt, labelList.length);
+    labelt.dispose()
 
     model = tf.sequential();
+
+    let hidden = tf.layers.dense({
+        units: 3,
+        activation: 'sigmoid',
+        inputDim: 3
+    });
+
+    let output = tf.layers.dense({
+        units: labelList.length,
+        activation: 'softmax',
+    });
+
     model.add(hidden);
     model.add(output);
+
+    const lr = 0.2;
+    const optimizer = tf.train.sgd(lr);
+
     model.compile({
         optimizer: optimizer,
-        loss: tf.losses.meanSquaredError
+        loss: 'categoricalCrossentropy'
     })
 
     train();
+    setInterval(()=>{
+        predict()
+    }, 500)
+}
+
+async function train() {
+    const config = {
+        shuffle: true,
+        epochs: 10000,
+        callbacks: {
+            onTrainBegin: () => console.log('training start'),
+            onTrainEnd: () => console.log('training complete'),
+            // onBatchEnd: tf.nextFrame,
+            onEpochEnd: (num, logs) => {
+                lossP.html('loss: ' + logs.loss)
+                epochP.html('epoch: ' + num)
+            }
+
+        }
+    }
+    return await model.fit(xs, ys, config)
 }
 
 function draw() {
-    background(255);
     
-    let ys = model.predict(xs);
-    let y_values = ys.dataSync();
-    ys.dispose();
+}
 
-    let index = 0;
-    for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < cols; j++) {
-            let br = y_values[index] * 255;
-            fill(br);
-            rect(resolution * j, resolution * i, resolution, resolution)
+function predict() {
+    let xs = tf.tensor2d(testColors)
+    let results = model.predict(xs);
+    let data = results.arraySync()
 
-            fill(255 - br);
-            textSize(12);
-            textAlign(CENTER, CENTER)
-            text(nf(y_values[index], 1, 2), resolution * j, resolution * i, resolution, resolution);
-            index++;
+    if (testColors.length > 0) {
+        let index = 0;
+        for (let i = 0; i < rows; i++) {
+            for (let j = 0; j < cols; j++) {
+                let color = testColors[index];
+                fill(color[0] * 255, color[1] * 255, color[2] * 255);
+                rect(resolution * j, resolution * i, resolution, resolution)
+
+                fill(255);
+                textSize(12);
+                textAlign(CENTER, CENTER)
+
+                let arr = data[index];
+                let indexOfMax = arr.indexOf(Math.max.apply(Math, arr))
+
+                let label = labelList[indexOfMax];
+                text(label, resolution * j, resolution * i, resolution, resolution);
+
+                textSize(10);
+                let label2 = `[${Math.floor(color[0] * 255)} ${Math.floor(color[1] * 255)} ${Math.floor(color[2] * 255)}]`
+                text(label2, resolution * j, resolution * i, resolution, resolution+30);
+                index++;
+            }
         }
     }
+    xs.dispose()
 }
+
+
